66d8303bd468c210af0ccda77e90060d2de06e94
Georgii Praskurin <prgeorge@mail.ru>
2025-08-03 19:15:30 +0700

    В модуль widget.py добавлена функция mask_account_card

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 """Основная программа"""
 
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card
 
-card = input("Введите номер карты")
-account = input("Введите номер счёта")
+#card = input("Введите номер карты")
+#account = input("Введите номер счёта")
+card_or_account = input("Введите название и номер карты или счёт")
 
-print(f"Замаскированный номер карты: {get_mask_card_number(card)}")
-print(f"Замаскированный номер счёта: {get_mask_account(account)}")
+#print(f"Замаскированный номер карты: {get_mask_card_number(card)}")
+#print(f"Замаскированный номер счёта: {get_mask_account(account)}")
+print(f"Новая функция {mask_account_card(card_or_account)}")
--- src/widget.py
+++ src/widget.py
@@ -1,2 +1,14 @@
-def mask_account_card(card_or_account :srt) -> str:
-    pass
+"""Модуль для маскировки номеров карт, номеров счетов или получения даты"""
+
+from src.masks import get_mask_card_number, get_mask_account
+
+def mask_account_card(card_or_account: str) -> str:
+    words_list = card_or_account.split()
+    stroka = ""
+    if words_list[0] == "Visa":
+        stroka = words_list[0] + " " + words_list[1] + " " + get_mask_card_number(words_list[2])
+    elif words_list[0] == "Maestro":
+        stroka = words_list[0] + " " + get_mask_card_number(words_list[1])
+    else:
+        stroka = words_list[0] + " " + get_mask_account(words_list[1])
+    return stroka
