1ba3a3fe1f788ce3ff7a6cf905e8fa970ba581f2
Georgii Praskurin <prgeorge@mail.ru>
2025-08-03 19:48:22 +0700

    Финальный commit. Добавлены описания новых функций

--- src/widget.py
+++ src/widget.py
@@ -4,6 +4,7 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card_or_account: str) -> str:
+    """Маскирока номера счёта или номера карты"""
     words_list = card_or_account.split()
     mask_str = ""
     if words_list[0] == "Счет":
@@ -14,5 +15,6 @@ def mask_account_card(card_or_account: str) -> str:
 
 
 def get_date(some_date: str) -> str:
+    """Вывод даты в форматированном виде"""
     date_in_russian = ".".join([some_date[8:10], some_date[5:7], some_date[:4]])
     return date_in_russian
