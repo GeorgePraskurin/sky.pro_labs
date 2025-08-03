b136fd4b06fcd4cdd05d14b4934a2b953544dcc0
Georgii Praskurin <prgeorge@mail.ru>
2025-08-03 19:44:05 +0700

    в модуль widget.py добавлена функция get_date

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 """Основная программа"""
 
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import mask_account_card
+from src.widget import get_date, mask_account_card
 
-#card = input("Введите номер карты")
-#account = input("Введите номер счёта")
-card_or_account = input("Введите название и номер карты или счёт")
+card = input("Введите номер карты: ")
+account = input("Введите номер счёта: ")
+card_or_account = input("Введите название и номер карты или счёт: ")
+random_date = input("Введите дату: ")
 
-#print(f"Замаскированный номер карты: {get_mask_card_number(card)}")
-#print(f"Замаскированный номер счёта: {get_mask_account(account)}")
+print(f"Замаскированный номер карты: {get_mask_card_number(card)}")
+print(f"Замаскированный номер счёта: {get_mask_account(account)}")
 print(f"Новая функция: {mask_account_card(card_or_account)}")
+print(f"А дата такая: {get_date(random_date)}")
--- src/widget.py
+++ src/widget.py
@@ -1,12 +1,18 @@
 """Модуль для маскировки номеров карт, номеров счетов или получения даты"""
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(card_or_account: str) -> str:
     words_list = card_or_account.split()
-    stroka = ""
+    mask_str = ""
     if words_list[0] == "Счет":
-        stroka = words_list[0] + " " + get_mask_account(words_list[1])
+        mask_str = words_list[0] + " " + get_mask_account(words_list[1])
     else:
-        stroka = ' '.join(words_list[:-1]) + " " + get_mask_card_number(words_list[-1])
-    return stroka
+        mask_str = " ".join(words_list[:-1]) + " " + get_mask_card_number(words_list[-1])
+    return mask_str
+
+
+def get_date(some_date: str) -> str:
+    date_in_russian = ".".join([some_date[8:10], some_date[5:7], some_date[:4]])
+    return date_in_russian
