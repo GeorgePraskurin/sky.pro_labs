ea4dd2d66cfc509967e0b7b70b8d0156e9dd02fc
Georgii Praskurin <prgeorge@mail.ru>
2025-08-24 12:14:25 +0700

    Добавлена функция filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,12 @@
-def filter_by_state:
-    pass
+def filter_by_state(list_of_dicts: list, filter_state = "EXECUTED") -> list:
+    """Фильтрация по элементу словаря state"""
+    filteres_list = []
+    for i in list_of_dicts:
+        if i["state"] == filter_state:
+            filteres_list.append(i)
+    return filteres_list
 
-def sort_by_date:
+def sort_by_date():
+    """Сортировка по дате"""
     pass
 
